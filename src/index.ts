559b32bb32d077b0d34d27184925542813f6c392
TheAbzo <abdelrahman.elferlay@gmail.com>
2022-06-20 05:09:51 +0200

    test: adding test initial structure & configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 console.log("sda")
+const myFunc = (num: number): number => {
+return num*num;
+};
+
+export default myFunc;
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,5 @@
+import myFunc from "../index";
+
+it('expect myFunc(5) to be = 25',()=>{
+    expect(myFunc(5)).toEqual(25);
+})
