50acbc451c5ee60f73d182b3d4a4183c44796521
TheAbzo <abdelrahman.elferlay@gmail.com>
2022-06-22 12:53:31 +0200

    fix: adding functions return types

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,16 +1,11 @@
 import app from '../index'
 import supertest from 'supertest'
-// import { response } from 'express';
 
 const request = supertest(app);
 
 describe("suite for testing the image endpoint response", () => {
-    //done for supertest to tell when our endpoint is done to disconnect from server
-    it('/api/images endpoint works well', async ()=>{
+    it('/api/images endpoint works well', async ():Promise<void> =>{
         const respone = await request.get('/api/images?filename=fjord&width=50&height=50')
-        
-        expect(respone.status).toBe(200);
-     
+        expect(respone.status).toBe(200);  
     })
-    // done();
 })
